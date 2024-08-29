82ce88259195b88789a3af6ee2e1d5c758a45c3d
LC-Dev <lordchristianagra23@gmail.com>
2024-08-29 21:35:28 +0800

    feat: applying 3d cards effects on area of expertise section

---
+++ components/Card3D.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+
+interface CardProps {
+  title: string;
+  description: string;
+  cardData: Array<string>;
+}
+
+const Card3D = ({ title, description, cardData }: CardProps) => {
+  return (
+    <CardContainer className="w-[80%] lg:w-[25rem]">
+      <CardBody className="bg-transparent border dark:border-white/[0.5] border-black/[0.5] rounded-xl relative dark:hover:shadow-2xl dark:hover:shadow-primaryRed/[0.5] hover:shadow-2xl hover:shadow-primaryBlack/[0.5] p-6">
+        <CardItem
+          as="h3"
+          translateZ="60"
+          className="text-xl font-bold text-primaryBlack dark:text-secondaryLightGray mb-1 font-display"
+        >
+          {title}
+        </CardItem>
+        <CardItem
+          as="h4"
+          translateZ="80"
+          className="text-base font-bold text-primaryBlack dark:text-accentSoftGray mb-4 font-special italic"
+        >
+          {description}
+        </CardItem>
+        <CardItem
+          as="div"
+          translateZ="100"
+          className="text-primaryBlack text-base mt-2 dark:text-secondaryLightGray font-sans"
+        >
+          <ul>
+            {cardData.map((listItem) => (
+              <li className="mb-2 pl-4 list-disc list-inside" key={title} dangerouslySetInnerHTML={{ __html: listItem }}></li>
+            ))}
+          </ul>
+        </CardItem>
+      </CardBody>
+    </CardContainer>
+  )
+}
+
+export default Card3D
--- components/Layouts/HomeLayout/AreasofExpertiseSection.tsx
+++ components/Layouts/HomeLayout/AreasofExpertiseSection.tsx
@@ -1,10 +1,46 @@
 import Section from '@/components/Section'
-import React from 'react'
+import Card3D from '@/components/Card3D';
+
+export const cardData = [
+    {
+        title: 'Website Development & SEO',
+        description: 'Building functional websites with a focus on improving search engine visibility.',
+        listItems: [
+            `<span class="text-primaryRed font-bold">Landing Page Implementation:</span> Converting designs into effective landing pages.`,
+            `<span class="text-primaryRed font-bold">SEO Best Practices:</span> Enhancing site visibility on search engines.`,
+            `<span class="text-primaryRed font-bold">Maintenance & Updates:</span> Keeping websites up-to-date and running smoothly.`,
+        ],
+    },
+    {
+        title: 'Front-End Web Development',
+        description: 'Creating reusable components and user-friendly interfaces for web-based systems.',
+        listItems: [
+            `<span class="text-primaryRed font-bold">HTML, CSS, JavaScript:</span> Building the visual and interactive parts of websites.`,
+            `<span class="text-primaryRed font-bold">React.js Applications:</span> Developing dynamic single-page applications.`,
+            `<span class="text-primaryRed font-bold">User Interface Implementation:</span> Converting UI/UX designs into functional websites.`,
+        ],
+    },
+    {
+        title: 'Mobile Development',
+        description: 'Developing cross-platform apps with a focus on user-friendly interfaces.',
+        listItems: [
+            `<span class="text-primaryRed font-bold">PWA:</span> Web apps that work offline, load quickly, and feel like native apps in a browser.`,
+            `<span class="text-primaryRed font-bold">Ionic React:</span> Creating hybrid mobile applications based on design specs.`,
+            `<span class="text-primaryRed font-bold">Mobile Optimization:</span> Ensuring apps perform well on both iOS and Android.`,
+        ],
+    },
+];
 
 const AreasofExpertiseSection = () => {
+
     return (
-        <Section>
-            <h2 className="text-center">Areas of Expertise</h2>
+        <Section className="py-20 lg:py-40 min-h-[50vh]">
+            <h2 className="text-2xl md:text-3xl font-semibold font-display text-center py-10"><span className="font-special relative after:absolute after:block after:w-[120%] after:h-16 after:bg-transparent after:border-4 after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-6 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 after:rotate-1">Areas of Expertise</span></h2>
+            <div className="flex flex-col lg:flex-row lg:flex-wrap gap-10 justify-center items-center">
+                {cardData.map((card) => (
+                    <Card3D key={card.title} title={card.title} description={card.description} cardData={card.listItems} />
+                ))}
+            </div>
         </Section>
     )
 }
