803cdaba8743e64a1cd8d5a1fda3a1b1261ded4a
LC-Dev <lordchristianagra23@gmail.com>
2024-08-29 21:40:29 +0800

    docs: adding visual representation to my hero section

--- components/Layouts/HomeLayout/HeroSection.tsx
+++ components/Layouts/HomeLayout/HeroSection.tsx
@@ -1,24 +1,30 @@
 import Section from '@/components/Section'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import Image from 'next/image'
 import { RiDownloadLine } from 'react-icons/ri'
+import { Button } from '@/components/ui/button'
+
+
 
 const HeroSection = () => {
   return (
-    <div className="dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative -z-10 flex items-center justify-center">
-      <div className="absolute -z-10 pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-
-      <Section className={"py-32 md:py-40 lg:py-0 flex flex-col-reverse items-center justify-end lg:flex-row lg:justify-center gap-10 md:gap-20 lg:gap-40"}>
-        <article className='max-w-[37rem]'>
-          <h1 className="text-3xl md:text-4xl font-semibold font-display"><span className="text-xl md:text-2xl font-normal font-special">Hi, <span className="text-primaryRed">Im</span> </span><br />Lord Christian Agra</h1>
-          <h2 className="text-xl md:text-2xl font-normal italic font-special pb-2 text-primaryRed">Web Developer</h2>
-          <p className="text-base md:text-xl font-normal font-sans">
+    <div className="flex items-center justify-center relative -z-10 dark:bg-black bg-white dark:bg-dot-primaryWhite/[0.3] bg-dot-primaryBlack/[0.3] ">
+      <div className="flex items-center justify-center absolute -z-10 dark:bg-black bg-white pointer-events-none inset-0  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <Section className="py-24 md:pt-40 lg:py-0 flex flex-col-reverse items-center justify-end lg:flex-row lg:justify-center gap-5 lg:gap-40">
+        <article className='lg:max-w-[30rem] xl:max-w-[40rem]'>
+          <h1 className="text-3xl md:text-4xl font-semibold font-display"><span className="text-xl md:text-2xl font-normal drop-shadow-2xl">Hi, <span className="text-primaryRed">Im</span> </span><br />Lord Christian Agra</h1>
+          <h2 className="text-xl md:text-2xl font-normal font-special italic pb-2 text-primaryRed ">Web Developer</h2>
+          <p className="text-base lg:text-lg font-normal font-sans">
             With 2 years of experience in building and maintaining responsive, user-friendly websites,
             I’ve gained a solid understanding of design principles and front-end technologies.
             I look forward to sharing how I create reliable, effective websites.</p>
           <Button className="mt-4 flex gap-1"><RiDownloadLine />Resume</Button>
         </article>
-        <div className="min-w-60 h-60 md:min-w-[20rem] md:h-[20rem] bg-red-600"></div>
+        <Image className="w-[10rem] md:w-[20rem] lg:w-[25rem] h-auto object-contain drop-shadow-2xl transform scale-x-[-1]"
+          src="/images/home-assets/picture-of-me.png"
+          width={1200}
+          height={1469}
+          alt="picture of LC Dev"
+          priority={true} />
       </Section>
     </div>
   )
