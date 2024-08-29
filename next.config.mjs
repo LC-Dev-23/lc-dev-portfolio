5199e01ecee834c7e55d052f5b2438b77d352894
LC-Dev <lordchristianagra23@gmail.com>
2024-08-29 23:17:14 +0800

    fix: turning off image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.lc-dev.site",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "lc-dev.site",
-        pathname: "**",
-      }
-    ],
+    unoptimized: true,
   },
+  //   images: {
+  //     remotePatterns: [
+  //       {
+  //         protocol: "https",
+  //         hostname: "www.lc-dev.site",
+  //         pathname: "**",
+  //       },
+  //       {
+  //         protocol: "https",
+  //         hostname: "lc-dev.site",
+  //         pathname: "**",
+  //       }
+  //     ],
+  //   },
 };
 
 export default nextConfig;
