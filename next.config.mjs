b96b6112adcbc643d82aafe8961cc53756b3887f
LC-Dev <lordchristianagra23@gmail.com>
2024-08-29 23:33:19 +0800

    fix: image not showing 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  //   images: {
-  //     remotePatterns: [
-  //       {
-  //         protocol: "https",
-  //         hostname: "www.lc-dev.site",
-  //         pathname: "**",
-  //       },
-  //       {
-  //         protocol: "https",
-  //         hostname: "lc-dev.site",
-  //         pathname: "**",
-  //       }
-  //     ],
-  //   },
-};
+const nextConfig = {};
 
 export default nextConfig;
