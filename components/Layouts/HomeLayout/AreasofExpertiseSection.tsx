42f4c2102b0f768c87a4cb61a9dbbbb63656d879
LC-Dev <lordchristianagra23@gmail.com>
2024-08-29 23:42:31 +0800

    style: making AE section and PP section 50% vh important and re uploading images

--- components/Layouts/HomeLayout/AreasofExpertiseSection.tsx
+++ components/Layouts/HomeLayout/AreasofExpertiseSection.tsx
@@ -34,7 +34,7 @@ export const cardData = [
 const AreasofExpertiseSection = () => {
 
     return (
-        <Section className="py-20 lg:py-40 min-h-[50vh]">
+        <Section className="py-20 lg:py-40 !min-h-[50vh]">
             <h2 className="text-2xl md:text-3xl font-semibold font-display text-center py-10"><span className="font-special relative after:absolute after:block after:w-[120%] after:h-16 after:bg-transparent after:border-4 after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-6 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 after:rotate-1">Areas of Expertise</span></h2>
             <div className="flex flex-col lg:flex-row lg:flex-wrap gap-10 justify-center items-center">
                 {cardData.map((card) => (
