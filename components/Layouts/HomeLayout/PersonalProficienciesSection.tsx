7350fc3868f42b0e7d9f60a83d8ef37da526abda
LC-Dev <lordchristianagra23@gmail.com>
2024-08-30 19:17:19 +0800

    docs: adding SoftskillCarousel component

--- components/Layouts/HomeLayout/PersonalProficienciesSection.tsx
+++ components/Layouts/HomeLayout/PersonalProficienciesSection.tsx
@@ -1,11 +1,11 @@
 import Section from '@/components/Section'
-import { MovingCards } from "@/components/MovingCards";
+import SoftSkillCarousel from '@/components/SoftSkillCarousel'
 
 const PersonalProficienciesSection = () => {
     return (
         <Section className="!min-h-[50vh] py-20">
-             <h2 className="text-2xl md:text-3xl font-semibold font-display text-center py-10 lg:pt-20"><span className="font-special relative after:absolute after:block after:w-[110%] after:h-16 after:bg-transparent after:border-4 after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-4 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 after:rotate-1">Personal Proficiencies</span></h2>
-             <MovingCards />
+            <h2 className="text-2xl md:text-3xl font-semibold font-display text-center py-10 lg:pt-20"><span className="font-special relative after:absolute after:block after:w-[110%] after:h-16 after:bg-transparent after:border-4 after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-4 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 after:rotate-1">Personal Proficiencies</span></h2>
+            <SoftSkillCarousel />
         </Section>
     )
 }
