6f0789460011d55824ac44ee732ec66806e79514
LC-Dev <lordchristianagra23@gmail.com>
2024-08-30 14:49:27 +0800

    docs: replacing services page to resume

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         { href: '/', text: 'HOME' },
         { href: '/about', text: 'ABOUT' },
         { href: '/project', text: 'PROJECT' },
-        { href: '/services', text: 'SERVICES' },
+        { href: '/resume', text: 'RESUME' },
         { href: '/contact', text: 'CONTACT', className: 'block lg:hidden' },
     ];
 
