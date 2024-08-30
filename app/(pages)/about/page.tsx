51b1ec0e0cc88772e5104c61074d8d6ac4287414
LC-Dev <lordchristianagra23@gmail.com>
2024-08-30 16:09:20 +0800

    docs: creating sections and heading to about pages

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,9 +1,18 @@
+import IntroductionSection from '@/components/Layouts/AboutLayout/IntroductionSection'
+import EducationalSection from '@/components/Layouts/AboutLayout/EducationalSection'
+import PersonalSection from '@/components/Layouts/AboutLayout/PersonalSection'
+import SummarySection from '@/components/Layouts/AboutLayout/SummarySection'
 import React from 'react'
+import CareerSection from '@/components/Layouts/AboutLayout/CareerSection'
 
 const AboutPage = () => {
   return (
     <main>
-      <h1>About Us</h1>
+      <IntroductionSection />
+      <EducationalSection />
+      <CareerSection />
+      <PersonalSection />
+      <SummarySection />
     </main>
   )
 }
---
+++ components/Layouts/AboutLayout/PersonalSection.tsx
@@ -0,0 +1,11 @@
+import Section from '@/components/Section'
+
+const PersonalSection = () => {
+  return (
+    <Section className="py-20 lg:py-40">
+      <h2 className="text-2xl md:text-3xl font-semibold font-display text-center py-10"><span className="font-special relative after:absolute after:block after:w-[115%] after:h-16 after:bg-transparent after:border-4 after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-6 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 after:rotate-1">Personal Enjoyments</span></h2>
+    </Section>
+  )
+}
+
+export default PersonalSection
