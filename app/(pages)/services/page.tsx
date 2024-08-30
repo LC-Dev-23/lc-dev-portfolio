3b29c37f3ff9c53a72e8a9320c8da03a911af212
LC-Dev <lordchristianagra23@gmail.com>
2024-08-30 16:13:09 +0800

    docs: turning resume page to services page and creating sections and heading

---
+++ app/(pages)/services/page.tsx
@@ -0,0 +1,16 @@
+import FAQSection from "@/components/Layouts/ServicesLayout/FAQSection"
+import WhatICanDoSection from "@/components/Layouts/ServicesLayout/WhatICanDoSection"
+import WhyMeSection from "@/components/Layouts/ServicesLayout/WhyMeSection"
+
+
+const ServicesPage = () => {
+  return (
+    <main>
+      <WhatICanDoSection />
+      <WhyMeSection />
+      <FAQSection />
+    </main>
+  )
+}
+
+export default ServicesPage
