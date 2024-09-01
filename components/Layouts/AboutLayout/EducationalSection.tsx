73fea09e4f5b5309dd0fb2ee544aa52dfb59b6d5
LC-Dev <lordchristianagra23@gmail.com>
2024-09-01 18:40:39 +0800

    style: removing space

--- components/Layouts/AboutLayout/EducationalSection.tsx
+++ components/Layouts/AboutLayout/EducationalSection.tsx
@@ -5,7 +5,7 @@ const EducationalSection = () => {
   return (
     <Section className="py-20 !min-h-[50vh]">
       <h2 className="text-2xl md:text-3xl font-semibold font-display text-center py-10"><span className="font-special relative after:absolute after:block after:w-[115%] after:h-16 after:bg-transparent after:border-4 after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-6 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 after:rotate-1">Educational Background</span></h2>
-      <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-5 lg:gap-10 ">
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-5 lg:gap-10">
         <article className='text-primaryBlack dark:text-accentSoftGray text-base lg:text-lg font-normal font-sans lg:max-w-[30rem] xl:max-w-[50rem] border dark:border-white/[0.2] border-black/[0.2] shadow-xl rounded-xl p-5 lg:p-10'>
           <p>
           <span className="text-accentBrightRed">Lord Christian Agra</span> earned his <span className="text-accentBrightRed">Bachelor of Science in Information Technology</span> from Philippine Best Training System Colleges,
