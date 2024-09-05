ae519b582f2ba92161f26c67ae8ed48f0f7b6b7b
LC-Dev <lordchristianagra23@gmail.com>
2024-09-05 08:39:08 +0800

    fix: adjustment of font weight for accessibility

--- components/Layouts/HomeLayout/HeroSection.tsx
+++ components/Layouts/HomeLayout/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <Section className="flex flex-col-reverse items-center justify-end lg:flex-row lg:justify-center md:gap-5 lg:gap-40 pt-28 pb-20 md:pt-40 lg:py-0">
       <article className='lg:max-w-[30rem] xl:max-w-[40rem]'>
         <h1 className="text-3xl md:text-4xl font-semibold font-display"><span className="text-lg md:text-xl font-normal drop-shadow-2xl">Hi, Im</span><br />Lord Christian Agra</h1>
-        <h2 className="text-xl md:text-2xl font-normal font-special italic pb-2 text-accentBrightRed">Web Developer</h2>
+        <h2 className="text-xl md:text-2xl font-bold font-special italic pb-2 text-accentBrightRed">Web Developer</h2>
         <p className="text-base lg:text-lg font-normal font-sans mb-5 lg:mb-10">
           With 2 years of experience in building and maintaining responsive, user-friendly websites,
           I’ve gained a solid understanding of design principles and front-end technologies.
