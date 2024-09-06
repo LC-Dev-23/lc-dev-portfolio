d4c039fbaf074d98e5e98aca1bf7381bb01ecd4d
LC-Dev <lordchristianagra23@gmail.com>
2024-09-06 13:27:23 +0800

    fix: not align centered

--- components/Layouts/HomeLayout/PersonalProficienciesSection.tsx
+++ components/Layouts/HomeLayout/PersonalProficienciesSection.tsx
@@ -1,12 +1,11 @@
 import H2 from '@/components/H2'
 import Section from '@/components/Section'
 import SoftSkillCarousel from '@/components/SoftSkillCarousel'
-import { Button } from '@/components/ui/moving-border'
 
 const PersonalProficienciesSection = () => {
     return (
-        <Section className="py-20 flex flex-col justify-center">
-            <div className="absolute h-screen w-[90%] min-h-[49rem] -z-20 dark:bg-grid-white/[0.1] bg-grid-black/[0.1]" />
+        <Section className="py-20 flex flex-col justify-center items-center">
+            <div className="absolute h-screen w-full min-h-[49rem] -z-20 dark:bg-grid-white/[0.1] bg-grid-black/[0.1]" />
             <div className="absolute h-screen w-full min-h-[49rem] -z-10 pointer-events-none inset-0 dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             <H2 borderStyle="after:w-[120%] md:after:w-[115%]">Personal Proficiencies</H2>
             <SoftSkillCarousel />
