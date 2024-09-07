e24ea9113d93315ca0bf0b17724f6b4cda7229a6
LC-Dev <lordchristianagra23@gmail.com>
2024-09-07 23:33:47 +0800

    style: replacing border and background

--- components/H2.tsx
+++ components/H2.tsx
@@ -7,8 +7,8 @@ interface H2Type {
 
 const H2 = ({ children, borderStyle }: H2Type) => {
     return (
-        <h2 className="text-xl md:text-2xl lg:text-3xl font-heading text-center py-10 pb-16">
-            <span className={`font-heading font-bold relative after:absolute after:block after:h-14 md:after:h-16 after:bg-transparent dark:after:shadow-none after:shadow-md after:border-4  after:border-primaryRed after:bottom-[-1rem] after:-z-10 after:-left-6 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 z-10  ${borderStyle}`}>
+        <h2 className="text-xl md:text-2xl lg:text-3xl font-heading text-center py-10">
+            <span className={`font-heading font-bold relative after:absolute after:block after:h-14 md:after:h-16 after:bg-transparent dark:after:shadow-none after:shadow-md after:border-4  after:border-accentDarkRed dark:after:border-accentLightRed after:bg-primaryWhite dark:after:bg-primaryBlack after:bottom-[-1rem] after:-z-10 after:-left-6 after:transition-transform after:duration-300 hover:after:transform after:skew-x-12 z-10 ${borderStyle}`}>
                 {children}
             </span>
         </h2>
