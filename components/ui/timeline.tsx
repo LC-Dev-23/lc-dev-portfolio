0ee9fea93815a6dcec6c908631575383e1ccd4a9
LC-Dev <lordchristianagra23@gmail.com>
2024-09-10 22:40:53 +0800

    style: Additional content and adjustment

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -34,7 +34,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full dark:bg-primaryBlack bg-primaryWhite border dark:border-white/[0.2] border-black/[0.2] dark:shadow-none shadow-md rounded-xl font-sans md:px-10 mt-5"
+      className="w-full dark:bg-primaryBlack bg-primaryWhite font-sans md:px-10 mt-5"
       ref={containerRef}
     >
       {/* <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
@@ -54,10 +54,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
+              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full dark:bg-white bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold font-heading text-primaryBlack/80 dark:text-primaryWhite">
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold font-heading text-primaryBlack/80 dark:text-primaryWhite/80">
                 {item.title}
               </h3>
             </div>
